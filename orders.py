d00d463a3120d23cda6e44acb68b2dc04c26a42e
dynamic-toast <jknap05@pm.me>
2025-09-27 23:38:04 -0400

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,33 +1,152 @@
 import requests
-import secretz
 
-CONTRACT_ID = secretz.CONTRACT_ID
-BASE_URL=secretz.BASE_URL
-ACCOUNT_ID = secretz.ACCOUNT_ID
+def long_order(jwt, account_id):
 
-def place_order(jwt, signal):
-
-    side_map = {"BUY": 0, "SELL": 1}
-    
-    url = f"{BASE_URL}/api/Order/place"
+    url ="https://api.topstepx.com/api/Order/place"
     headers = {
         "Authorization": f"Bearer {jwt}",
         "Accept": "text/plain",
         "Content-Type": "application/json"
     }
     payload = {
-        "accountId": ACCOUNT_ID,
-        "contractId": CONTRACT_ID,
-        "type": 1,
-        "side": side_map[signal["side"]],
-        "size": 5,
-        "limitPrice": signal.get("price"),
+        "accountId": account_id,
+        "contractId": "CON.F.US.GCE.Z25",
+        "type": 2,
+        "side": 0,
+        "size": 1,
+        "stopLossBracket": {
+        "ticks": -50,
+        "type": 4
+        },
+        "takeProfitBracket": {
+        "ticks": 90,
+        "type": 1
+        }
+    }
+
+
+    try:
+        response_order = requests.post(url, json=payload, headers=headers, timeout=15)
+        response_order.raise_for_status()
+        response_order = response_order.json() 
+        return response_order
+    except Exception as e:
+        return print("Error: ", e)
+    
+def short_order(jwt, account_id):
+    url ="https://api.topstepx.com/api/Order/place"
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Accept": "text/plain",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "accountId": account_id,
+        "contractId": "CON.F.US.GCE.Z25",
+        "type": 2,
+        "side": 1,
+        "size": 1,
+        "stopLossBracket": {
+        "ticks": 50,
+        "type": 4
+        },
+        "takeProfitBracket": {
+        "ticks": -90,
+        "type": 1
+        }
     }
 
     try:
-        response_buy = requests.post(url, json=payload, headers=headers, timeout=15)
-        response_buy.raise_for_status()
-        response_buy = response_buy.json() 
-        return response_buy
+        response_order = requests.post(url, json=payload, headers=headers, timeout=15)
+        response_order.raise_for_status()
+        response_order = response_order.json() 
+        return response_order
     except Exception as e:
         return print("Error: ", e)
+    
+
+def buy_market(jwt, account_id):
+    url ="https://api.topstepx.com/api/Order/place"
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Accept": "text/plain",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "accountId": account_id,
+        "contractId": "CON.F.US.GCE.Z25",
+        "type": 2,
+        "side": 0,
+        "size": 1,
+
+        "stopLossBracket": {
+        "ticks": -50,
+        "type": 4
+        },
+        "takeProfitBracket": {
+        "ticks": 90,
+        "type": 1
+        }
+    }
+
+    try:
+        response_order = requests.post(url, json=payload, headers=headers, timeout=15)
+        response_order.raise_for_status()
+        response_order = response_order.json() 
+        print("LOG: ORDER FILLED ------- 1 buy market filled")
+        return response_order
+    except Exception as e:
+        return print("Error: ", e)
+
+def sell_market(jwt, account_id):
+    url ="https://api.topstepx.com/api/Order/place"
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Accept": "text/plain",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "accountId": account_id,
+        "contractId": "CON.F.US.GCE.Z25",
+        "type": 2,
+        "side": 1,
+        "size": 1,
+
+        "stopLossBracket": {
+        "ticks": 50,
+        "type": 4
+        },
+        "takeProfitBracket": {
+        "ticks": -90,
+        "type": 1
+        }
+    }
+
+    try:
+        response_order = requests.post(url, json=payload, headers=headers, timeout=15)
+        response_order.raise_for_status()
+        response_order = response_order.json() 
+        print("LOG: ORDER FILLED ------- 1 sell market filled", response_order)
+        return response_order
+    except Exception as e:
+        return print("Error: ", e)
+    
+def flatten(jwt, account_id):
+
+    url = "https://api.topstepx.com/api/Position/closeContract"
+    headers = {
+        "Authorization": f"Bearer {jwt}",
+        "Accept": "text/plain",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "accountId": account_id,
+        "contractId": "CON.F.US.GCE.Z25"
+    }
+
+    response_order = requests.post(url, headers=headers, json=payload)
+    response_order.raise_for_status()
+    response_order = response_order.json()
+    print("LOG: CLOSING POSITIONS ------- ", response_order)
+    return response_order
